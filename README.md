bfd2f4c2279deaf5d4592d4c491da51c428213e2
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-09-20 14:15:54 +0200

    Allow to define also the host

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 ```
 const { app, express } = require('mxd-express')(config);
-// if the port is later needed use 'config.port' 
+// if the port is later needed use 'config.port'
+// if the host is later needed use 'config.host'
 ```
--- src/lib.js
+++ src/lib.js
@@ -3,14 +3,15 @@
 const express = require('express');
 
 module.exports = config => {
-  config.port = process.env.PORT || config.port;
+  config.port = process.env.PORT || process.env.MXD_PORT || config.port;
+  config.host = process.env.MXD_HOST || config.host;
 
   const app = express();
   app.set('x-powered-by', false);
   app.get('/ping', (req, res) => {
     res.send();
   });
-  app.listen(config.port);
+  app.listen(config.port, config.host);
 
   return { app: app, express: express };
 };
