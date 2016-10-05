265ff6df3d2ebb502f64bdfb6b23a895771e4981
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-10-05 13:23:59 +0200

    Default value for the port

--- src/lib.js
+++ src/lib.js
@@ -3,7 +3,7 @@
 const express = require('express');
 
 module.exports = config => {
-  config.port = process.env.PORT || process.env.MXD_PORT || config.port;
+  config.port = process.env.PORT || process.env.MXD_PORT || config.port || 80;
   config.hostname = process.env.MXD_HOSTNAME || config.hostname;
 
   const app = express();
